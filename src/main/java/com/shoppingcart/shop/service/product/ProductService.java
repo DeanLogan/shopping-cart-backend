eaa5ab77ee4be9628134d49b76ba5f668457a714
Dean Logan <deanlogan42@gmail.com>
2024-09-13 17:18:45 +0100

    Added product service class along with interface for this class and ProductRepository

---
+++ src/main/java/com/shoppingcart/shop/service/product/ProductService.java
@@ -0,0 +1,69 @@
+package com.shoppingcart.shop.service.product;
+
+import com.shoppingcart.shop.exceptions.ProductNotFoundException;
+import com.shoppingcart.shop.model.Product;
+import com.shoppingcart.shop.service.repository.ProductRepository;
+
+import java.util.List;
+
+public class ProductService implements IProductService{
+    private ProductRepository productRepository;
+
+    @Override
+    public Product getProductById(Long id) {
+        return productRepository.findById(id)
+                .orElseThrow(()-> new ProductNotFoundException("Product not found"));
+    }
+
+    @Override
+    public Product addProduct(Product product) {
+        return null;
+    }
+
+    @Override
+    public void deleteProductById(Long id) {
+        productRepository.findById(id).ifPresentOrElse(productRepository::delete, () -> {
+            throw new ProductNotFoundException("Product not found");
+        });
+    }
+
+    @Override
+    public void updateProduct(Product product, Long productId) {
+
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public List<Product> getProductsByCategory(String category) {
+        return productRepository.findByCategoryName(category);
+    }
+
+    @Override
+    public List<Product> getProductsByBrand(String brand) {
+        return productRepository.findByBrand(brand);
+    }
+
+    @Override
+    public List<Product> getProductsByCategoryAndBrand(String category, String brand) {
+        return productRepository.findByCategoryNameAndBrand(category, brand);
+    }
+
+    @Override
+    public List<Product> getProductsByName(String name) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductByBrandAndName(String brand, String name) {
+        return List.of();
+    }
+
+    @Override
+    public Long countProductsByBrandAndName(String brand, String name) {
+        return 0L;
+    }
+}
