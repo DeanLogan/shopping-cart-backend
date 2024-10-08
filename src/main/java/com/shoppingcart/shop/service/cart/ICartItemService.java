3747e4d778914e0eec028e2c36786f586b5a806a
Dean Logan <deanlogan42@gmail.com>
2024-10-08 15:29:50 +0100

    Added implementation for CartItemService

--- src/main/java/com/shoppingcart/shop/service/cart/ICartItemService.java
+++ src/main/java/com/shoppingcart/shop/service/cart/ICartItemService.java
@@ -1,4 +1,11 @@
 package com.shoppingcart.shop.service.cart;
 
+import com.shoppingcart.shop.model.CartItem;
+
 public interface ICartItemService {
+    void addItemToCart(Long cartId, Long productId, int quantity);
+    void removeItemFromCart(Long cartId, Long productId, int quantity);
+    void updateItemQuantity(Long cartId, Long productId, int quantity);
+
+    CartItem getCartItem(Long cartId, Long productId);
 }
